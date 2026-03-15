4871fcb172ab470b814d1e7aba23d43b3e5ce1b8
fylox-protocol <fyloxprotocol@gmail.com>
2026-03-15 01:19:08 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1211,7 +1211,7 @@ if (id === 's8') {
 }
 // Mostrar/ocultar bnav según la pantalla
   const bnav = document.querySelector('.bnav');
-  const navScreens = ['s5','s6','s8','s9','s10','s11','s12','s13','s14','s15','s16','s17','s18','s19','s20','s21','s22','s23','s24'];
+  const navScreens = ['s5','s8','s9','s10','s11','s12','s13','s14','s15','s16','s17','s18','s19','s20','s21','s22','s23','s24'];
   if (bnav) {
     bnav.style.display = navScreens.includes(id) ? 'flex' : 'none';
   }
