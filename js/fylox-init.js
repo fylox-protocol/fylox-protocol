d3c7cb9bff42dbae68f0b80e92c3d07f97e48d5e
fylox-protocol <fyloxprotocol@gmail.com>
2026-04-04 13:06:02 -0300

    Update fylox-init.js

--- js/fylox-init.js
+++ js/fylox-init.js
@@ -5,15 +5,6 @@
 document.addEventListener('DOMContentLoaded', async function() {
 
   try {
-      // Inicializar el SDK de Pi Network
-      // Nota: Recuerda poner sandbox: false cuando vayas a Mainnet
-      Pi.init({ version: "2.0", sandbox: false }); 
-      console.log("[Fylox] Pi SDK inicializado correctamente");
-  } catch (err) {
-      console.error("[Fylox] Error al inicializar Pi SDK:", err);
-  }
-
-  try {
     const savedLang    = FyloxStorage.get('fylox-lang');
     const detectedLang = (savedLang && LANGS[savedLang]) ? savedLang : detectLang();
     applyLang(detectedLang);
